fab694488159938ac57bdbd43ea6c2c7431c5efb
Tavara Angelo <angelotavara07@gmail.com>
2026-06-03 15:09:41 -0500

    Soy Angelo Távara Rabelo y he agregado la función de que el Login solo acepte las credenciales ubicadas en el archivo users.js de la carpeta data, de lo contrario no permitira al usuario ingresar, adicionalmente instale sweet alert 2 para mejorar el visual de los mensajes de credenciales correctas e incorrectas.

---
+++ src/data/users.js
@@ -0,0 +1,18 @@
+export const users = [
+    {
+      email: "admin@stockflow.com",
+      password: "admin123",
+      rol: "Administrador"
+    },
+    {
+      email: "admin2@stockflow.com",
+      password: "admin456",
+      rol: "Administrador"
+    },
+    {
+      email: "admin3@stockflow.com",
+      password: "admin789",
+      rol: "Administrador"
+    }
+  ];
+  
